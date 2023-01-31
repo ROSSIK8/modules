6f8b5c9a05ecdadb96c8edfac3dd2dcc0379cf31
Sanzhay Ayurov <ayurov2004@gmail.com>
2023-02-01 08:00:48 +0900

    Change bs4

--- bs4_.py
+++ bs4_.py
@@ -11,10 +11,6 @@ counter = 0
 
 print('Топ 10 фильмов:')
 for item in data:
-    link = item.find('a', class_="movieItem_title").get('href')
-    res = requests.get(link)
-    soup_ = BeautifulSoup(res.text, 'lxml')
-    data_ = soup_.find('div', class_="filmInfo_column grid_cell9")
 
     name = item.find('a', class_="movieItem_title").text
     rating = item.find('span', class_="rating_num").text + '⭐️'
